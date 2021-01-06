92d39830e7fe94e3a4b7effc8ddec7c13e65cf73
Christian Albrecht <christian.albrecht@mayflower.de>
2021-01-06 21:20:20 +0100

    initial vomit

---
+++ flake.nix
@@ -0,0 +1,26 @@
+{
+  description = "Local nixpkgs overlays flake";
+
+  inputs = {
+    emacs = { url = path:./emacs; };
+    firefox-nightly = { url = path:./firefox-nightly; inputs.nixpkgs.follows = "nixpkgs"; };
+    global-cursor-theme = { url = path:./global-cursor-theme; };
+    nodejs = { url = path:./nodejs; inputs.nixpkgs.follows = "nixpkgs"; };
+    rust = { url = path:./rust; };
+    wayland = { url = github:colemickens/nixpkgs-wayland; inputs.nixpkgs.follows = "nixpkgs"; };
+  };
+
+  outputs = { self, nixpkgs, ... }@inputs:
+    let
+      system = "x86_64-linux";
+      pkgs = import nixpkgs {
+        inherit system;
+        overlays = with self.overlays; [ rust wayland nodejs emacs firefox-nightly ];
+      };
+    in
+    {
+      legacyPackages."${system}" = pkgs;
+
+      overlays = nixpkgs.lib.mapAttrs (_: input: input.overlay) inputs;
+    };
+}
