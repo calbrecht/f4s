f7aeed2508e3c3ed0629294b97a752c0d59769ab
Christian Albrecht <christian.albrecht@mayflower.de>
2021-01-25 15:25:40 +0100

    Allow unfree for google-chrome

--- flake.nix
+++ flake.nix
@@ -19,6 +19,7 @@
       system = "x86_64-linux";
       pkgs = import nixpkgs {
         inherit system;
+        config = { allowUnfree = true; };
         overlays = with self.overlays; [
           nix-zsh-completions
           rust
