cba176c371556dfb969b161e8f96af6ceae2d86e
Christian Albrecht <christian.albrecht@mayflower.de>
2023-02-25 06:18:12 +0100

    add home-manager remove silo

--- flake.nix
+++ flake.nix
@@ -36,6 +36,10 @@
       url = github:nix-community/nixpkgs-wayland;
       inputs.nixpkgs.follows = "nixpkgs";
     };
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
   outputs = { self, nixpkgs, ... }@inputs:
@@ -65,54 +69,6 @@
         vscode = pkgs.vscode-with-extensions.override {
           vscodeExtensions = with pkgs.vscode-extensions; [ ms-vsliveshare.vsliveshare ];
         };
-
-        silo = with pkgs; with libsForQt5; stdenv.mkDerivation {
-          pname = "silo";
-          version = "git-2021-09-19-78ba44abe8";
-          src = fetchfossil {
-            url = "https://code.jessemcclure.org/silo/";
-            rev = "78ba44abe8";
-            sha256 = "sha256-nfp520cdS9bUGQv8AfN5dK60YCmkM1gQDL8FTA4WqeM=";
-          };
-          nativeBuildInputs = [ wrapQtAppsHook pkgconf ];
-          buildInputs = [
-            qtbase
-            wayland
-            layer-shell-qt
-          ];
-          patchPhase = ''
-            substituteInPlace ./Makefile \
-            --replace '/usr/include/LayerShellQt' '${layer-shell-qt}/include/LayerShellQt' \
-            --replace '= /usr' '= ${placeholder "out"}'
-
-            substituteInPlace ./src/barwin.cpp \
-            --replace \
-            'case C | Key_Q:          end(0);            break;' \
-            'case C | Key_G:          end(0);            break;
-             case C | Key_N:          sel(+1);           break;
-             case C | Key_P:          sel(-1);           break;
-             case C | Key_F:          move(+1);          break;
-             case C | Key_B:          move(-1);          break;
-             case C | Key_E:          move(+MAX);        break;
-             case C | Key_A:          move(-MAX);        break;
-             case C | Key_D:          del(+1);           break;
-             case C | Key_H:          del(-1);           break;
-             case C | Key_K:          del(+MAX);         break;
-             case C | A | Key_H:      del(-MAX);         break;
-             case C | Key_M:          end(1);            break;
-             case C | A | Key_M:      end(2);            break;
-             case C | Key_Q:          end(0);            break;' \
-            --replace \
-            'case     Key_Tab:        sel(+1);           break;' \
-            'case     Key_Tab:        complete(0);       break;'
-          '';
-          installTargets = "install-extra";
-          postInstall = ''
-            #mkdir -p $out/etc/xdg
-            #cp -R $out/share/doc/silo $out/etc/xdg/
-          '';
-          outputs = [ "out" "dev" ];
-        };
       };
 
       defaultPackage."${system}" = pkgs.nix-zsh-completions;
@@ -133,6 +89,10 @@
             });
           };
         });
+      };
+
+      nixosModules = {
+        inherit (inputs.home-manager.nixosModules) home-manager;
       };
     };
 }
