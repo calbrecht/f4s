1b82ec1c2475b60d1520477b162dfaf7ac142ff3
Christian Albrecht <christian.albrecht@mayflower.de>
2023-07-23 21:45:32 +0200

    remove home configuration and use flake-utils

--- flake.nix
+++ flake.nix
@@ -6,6 +6,11 @@
   };
 
   inputs = {
+    systems.url = github:nix-systems/x86_64-linux;
+    flake-utils = {
+      url = github:numtide/flake-utils;
+      inputs.systems.follows = "systems";
+    };
     emacs = {
       url = flake:f4s-emacs;
       inputs.nixpkgs.follows = "nixpkgs";
@@ -36,72 +41,45 @@
       url = github:nix-community/nixpkgs-wayland;
       inputs.nixpkgs.follows = "nixpkgs";
     };
-    home = {
-      url = path:/f4s/home;
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
   };
 
-  outputs = { self, nixpkgs, ... }@inputs:
-    let
-      system = "x86_64-linux";
-      pkgs = import nixpkgs {
-        inherit system;
-        config = {
-          allowUnfree = true;
-          permittedInsecurePackages = [
-            "python2.7-urllib3-1.26.2"
-            "python2.7-pyjwt-1.7.1"
-          ];
-        };
-        overlays = with self.overlays; [
-          fixups
-          rust
-          wayland
-          nodejs
-          emacs
-          firefox-nightly
+  outputs = { self, nixpkgs, flake-utils, ... }@inputs:
+  {
+    overlays = nixpkgs.lib.mapAttrs (_: i: i.overlays.default or i.overlay) inputs;
+  }
+  // (flake-utils.lib.eachDefaultSystem (system: let
+    pkgs = import nixpkgs {
+      inherit system;
+      config = {
+        allowUnfree = true;
+        permittedInsecurePackages = [
+          "python2.7-urllib3-1.26.2"
+          "python2.7-pyjwt-1.7.1"
         ];
       };
-    in
-    {
-      legacyPackages."${system}" = pkgs // {
-        vscode = pkgs.vscode-with-extensions.override {
-          vscodeExtensions = with pkgs.vscode-extensions; [ ms-vsliveshare.vsliveshare ];
-        };
-      };
-
-      defaultPackage."${system}" = pkgs.nix-zsh-completions;
-
-      overlays = with nixpkgs.lib; recursiveUpdate (mapAttrs (_: input: input.overlay) inputs) {
-        wayland = (self: prev:
-        let
-          selfWaylandPkgs = inputs.wayland.overlays.default self prev ;
-        in
-        {
-          waylandPkgs = recursiveUpdate selfWaylandPkgs {
-            swaylock = selfWaylandPkgs.swaylock.overrideAttrs (old: {
-              mesonFlags = [
-                "-Dpam=enabled"
-                "-Dgdk-pixbuf=enabled"
-                "-Dman-pages=enabled"
-              ];
-            });
-          };
-        });
-      };
-
-      nixosModules = inputs.home.nixosModules { inherit pkgs; };
-
-      homeConfigurations = inputs.home.homeConfigurations { inherit pkgs; };
-
-      apps."${system}".hm = {
-        type = "app";
-        program = (pkgs.writeScriptBin "run-home-manager.sh" ''
-          nix flake lock --update-input home \
-          && nix run ./#home-manager -- "''${@}"
-        '') + /bin/run-home-manager.sh;
-      };
-
+      overlays = with self.overlays; [
+        fixups
+        rust
+        wayland
+        nodejs
+        emacs
+        firefox-nightly
+      ];
     };
+  in {
+    apps = {
+      hm = flake-utils.lib.mkApp {
+        drv = (pkgs.writeScriptBin "run-home-manager.sh" ''
+          pushd /f4s/home
+          nix run home-manager -- --flake ./ "''${@}"
+        '');
+      };
+    };
+    packages = { default = pkgs.hello; };
+    legacyPackages = pkgs // {
+      vscode = pkgs.vscode-with-extensions.override {
+        vscodeExtensions = with pkgs.vscode-extensions; [ ms-vsliveshare.vsliveshare ];
+      };
+      };
+  }));
 }
